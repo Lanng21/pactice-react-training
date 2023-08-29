e3df1bb288b0a44bb4e8a8a69beee097a448dd25
Nguyen Mai Lan <103887524+Lanng21@users.noreply.github.com>
2023-08-29 17:18:34 +0700

    Feat/implement homepage (#4)
    * feat: implement homepage
    
    * update json file
    
    * update: memo for table
    
    * update dedependencies useCallback and button custom

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,43 +1,148 @@
-import React, { useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
+
+// hooks
+import { useProductContext } from '../../hooks/ProductContext';
+
 // components
-import ModalForm from '../../components/Form';
 import Button from '../../components/Button';
+import Table from '../../components/Table';
+import DeleteConfirmModal from '../../components/DeleteConfirmModal';
+import DeleteSuccessModal from '../../components/DeleteSuccessModal';
+import AddProduct from './components/AddProduct';
+
 // types
 import { IProduct } from '../../types/Product';
-import Table from '../../components/Table';
 
-function Home() {
+const Home = () => {
+  const { products, addProduct, updateProduct, deleteProduct } =
+    useProductContext();
+
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [products, setProducts] = useState<IProduct[]>([]);
 
-  const openModal = () => {
+  const [selectedProduct, setSelectedProduct] = useState<IProduct | null>(null);
+
+  const [isDeleteConfirmationModalOpen, setIsDeleteConfirmationModalOpen] =
+    useState(false);
+
+  const [isSuccessModalOpen, setIsSuccessModalOpen] = useState(false);
+
+  const handleConfirmDelete = useCallback(() => {
+    setIsDeleteConfirmationModalOpen(false);
+    if (selectedProduct) {
+      deleteProduct(selectedProduct.id);
+      setSelectedProduct(null);
+      setIsSuccessModalOpen(true);
+    }
+  }, [deleteProduct, selectedProduct]);
+
+  const openModal = useCallback(() => {
     setIsModalOpen(true);
-  };
+  }, []);
 
-  const closeModal = () => {
+  const closeModal = useCallback(() => {
     setIsModalOpen(false);
-  };
+    setSelectedProduct(null);
+  }, []);
 
-  const handleSubmit = (formData: IProduct) => {
-    const newProduct = {
-      ...formData,
-      id: Date.now(),
-    };
-    setProducts((prevProducts) => [...prevProducts, newProduct]);
-  };
+  const handleSubmit = useCallback(
+    (formData: IProduct) => {
+      if (selectedProduct) {
+        updateProduct({ ...selectedProduct, ...formData });
+      } else {
+        const newProduct: IProduct = {
+          ...formData,
+          id: Date.now(),
+        };
+        addProduct(newProduct);
+      }
+      closeModal();
+    },
+    [addProduct, closeModal, selectedProduct, updateProduct],
+  );
+
+  const handleEdit = useCallback((product: IProduct) => {
+    setSelectedProduct(product);
+    setIsModalOpen(true);
+  }, []);
+
+  const columns = useMemo(
+    () => [
+      {
+        key: 'name',
+        header: 'Product',
+        render: (product: IProduct) => product.name,
+      },
+      {
+        key: 'status',
+        header: 'Status',
+        render: (product: IProduct) => product.status,
+      },
+      {
+        key: 'type',
+        header: 'Type',
+        render: (product: IProduct) => product.type,
+      },
+      {
+        key: 'quantity',
+        header: 'Quantity',
+        render: (product: IProduct) => product.quantity,
+      },
+      {
+        key: 'brand',
+        header: 'Brand',
+        render: (product: IProduct) => product.brand,
+      },
+      {
+        key: 'price',
+        header: 'Price',
+        render: (product: IProduct) => product.price,
+      },
+      {
+        key: 'action',
+        header: 'Action',
+        render: (product: IProduct) => (
+          <div className="action-buttons">
+            <Button onClick={() => handleEdit(product)}>edit</Button>
+            <Button
+              onClick={() => {
+                setSelectedProduct(product);
+                setIsDeleteConfirmationModalOpen(true);
+              }}
+            >
+              delete
+            </Button>
+          </div>
+        ),
+      },
+    ],
+    [handleEdit],
+  );
 
   return (
     <div className="container">
-      <h1>Product Management</h1>
-      <Button onClick={openModal}>Open Modal</Button>
+      <h1 className="title">Management</h1>
+      <div className="button-add">
+        <Button onClick={openModal}>Add new product</Button>
+      </div>
       {isModalOpen && (
-        <div className="overlay">
-          <ModalForm onClose={closeModal} onSubmit={handleSubmit} />
-        </div>
+        <AddProduct
+          onClose={closeModal}
+          onSubmit={handleSubmit}
+          selectedProduct={selectedProduct as IProduct}
+        />
       )}
-      <Table products={products} />
+      {isDeleteConfirmationModalOpen && (
+        <DeleteConfirmModal
+          onClose={() => setIsDeleteConfirmationModalOpen(false)}
+          onConfirm={handleConfirmDelete}
+        />
+      )}
+      {isSuccessModalOpen && (
+        <DeleteSuccessModal onClose={() => setIsSuccessModalOpen(false)} />
+      )}
+      <Table columns={columns} data={products} />
     </div>
   );
-}
+};
 
 export default Home;
